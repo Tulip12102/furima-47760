35d18d7e30f5e51b519ee55dfd827e351cda0178
Tulip12102 <yamamotosayaka1121@gmail.com>
2026-04-17 22:10:08 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,14 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :delivery_day
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ },
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :user_id, presence: true
+  validates :image, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
