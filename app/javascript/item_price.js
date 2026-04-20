b0befd468d0e82a13756b3e2ef0b4aa2bfdadfcf
Tulip12102 <yamamotosayaka1121@gmail.com>
2026-04-20 17:11:16 +0900

    出品失敗時に再度JSを動作させる

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,11 +1,17 @@
-window.addEventListener('turbo:load', () => {
+const price = () => {
   const priceInput = document.getElementById("item-price");
+  if (!priceInput) return;
+
   priceInput.addEventListener("input", () => {
-      const inputValue = priceInput.value;
-      const addTaxDom = document.getElementById("add-tax-price");
-      const tax = Math.floor( inputValue * 0.1 );
-      addTaxDom.innerHTML = tax;
-      const profit = document.getElementById("profit");
-      profit.innerHTML = Math.floor( inputValue * 0.9 );
+    const inputValue = Number(priceInput.value);
+    const addTaxDom = document.getElementById("add-tax-price");
+    const tax = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = tax;
+
+    const profit = document.getElementById("profit");
+    profit.innerHTML = Math.floor(inputValue * 0.9);
   });
-});
+};
+
+document.addEventListener("turbo:load", price);
+document.addEventListener("turbo:render", price);
