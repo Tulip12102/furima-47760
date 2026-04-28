0025c9713e1b176ba4e603aed5a90647339045c0
Tulip12102 <yamamotosayaka1121@gmail.com>
2026-04-28 12:13:41 +0900

    ログイン有無での遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :redirect_if_invalid_item, only: [:index, :create]
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
@@ -21,5 +24,15 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(
       :postal_code, :prefecture_id, :city, :address_line, :building_name, :phone_number, :token
     ).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_if_invalid_item
+    return unless @item.order.present? || current_user == @item.user
+
+    redirect_to root_path
   end
 end
