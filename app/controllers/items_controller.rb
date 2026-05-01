c7b7013a13569942842564edaa1a3a03d23a12f8
Tulip12102 <yamamotosayaka1121@gmail.com>
2026-05-01 15:53:32 +0900

    出品者のみ商品の編集ができる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,9 +47,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return unless @item.order.present?
-
-    redirect_to root_path
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def set_item
